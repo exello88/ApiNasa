f527c5dda620407b100a210b4028ed0d47ddff5e
ex <99123237+exello88@users.noreply.github.com>
2024-04-04 14:35:11 +0300

    первое изменение ошибок указанных в pull request(script.js)

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
-let MainText = document.querySelector('.main__text');
-let Description = document.querySelector('.description');
-const APODbtn = document.querySelector('.APOD_btn');
-const NeoWsbtn = document.querySelector('.NeoWs_btn');
-const EONETbtn = document.querySelector('.EONET_btn');
-const Satellitebtn = document.querySelector('.Satellite_btn');
-const InSightbtn = document.querySelector('.InSight_btn');
-let TypeOfBtn = 0;
+let mainText = document.querySelector('.main__text');
+let description = document.querySelector('.description');
+const apodBtn = document.querySelector('.APOD_btn');
+const neowsBtn = document.querySelector('.NeoWs_btn');
+const eonetBtn = document.querySelector('.EONET_btn');
+const satelliteBtn = document.querySelector('.Satellite_btn');
+const insightBtn = document.querySelector('.InSight_btn');
+const url = 'https://api.nasa.gov/';
+const apiKey = '8MwhBFzZBekhCEakGeorXN0pRZH2K57zgoMGcL5C';
+let typeOfBtn = 0,numLastHandler=0;
 
-function ApiApod(data) {
+
+
+function checkAgainClick(numberThisTab) {
+    switch (typeOfBtn) {
+        case numberThisTab: return false;
+        default: return true;
+    }
+}
+
+function checkServerResponse(response) {
+    if (response.status >= 400 && response.status <= 500) {
+        alert('Ошибка ответа сервера. Error' + response.status);
+    }
+    else if (response.status === 200) return true;
+}
+
+function apiApod(date) {
     const descriptionBlockImg = document.querySelector('.description__img')
     const descriptionBlock = document.querySelector('.description__block');
-    fetch('https://api.nasa.gov/planetary/apod?api_key=8MwhBFzZBekhCEakGeorXN0pRZH2K57zgoMGcL5C&date=' + data.value).then((response) => response.json()).then((response) => {
-        if (response.explanation === undefined) {
-            alert('Нет информации по заданной дате');
-        }
-        else {
-            descriptionBlockImg.setAttribute('src', response.url);
-            descriptionBlock.textContent = response.explanation;
+    fetch(url + 'planetary/apod?api_key=' + apiKey + '&date=' + date.value).then((response) => {
+        if (checkServerResponse(response)) {
+            response.json().then((response) => {
+                descriptionBlockImg.setAttribute('src', response.url);
+                descriptionBlock.textContent = response.explanation;
+            })
         }
     });
 }
 
-APODbtn.addEventListener('click', () => {
-    if (TypeOfBtn !== 1) {
-        MainText.textContent = 'Получите изображение астрономического обьекта на указанную дату:';
-        MainText.innerHTML += '<input type="date" class="input__date" onkeydown="return false">';
-        Description.innerHTML += `
+function deleteLastHandler(date) {
+    //Потом, когда буду дорабатывать все остальные табы более точно опишу параметры в цункциях, потому что они вероятно будут другие, пока что так накидал просто
+    switch (numLastHandler) {
+        case 1:
+            date.removeEventListener('change', apiApod(date));
+            break;
+        case 2:
+            date.removeEventListener('change', apiNeows(date));
+            break;
+        case 3:
+            date.removeEventListener('change', apiEonet(date));
+            break;
+        case 4:
+            date.removeEventListener('change', apiSatellite(date));
+            break;
+        case 5:
+            date.removeEventListener('change', apInsight(date));
+            break;
+    }
+}
+
+apodBtn.addEventListener('click', () => {
+    if (checkAgainClick(1)) {
+        mainText.innerHTML = 'Получите изображение астрономического обьекта на указанную дату:<input type="date" class="input__date" onkeydown="return false">';
+        description.innerHTML = `
         <div class="description__BlockImg"><img class="description__img" alt="Фото"></div>
         <div class="description__block"></div>
         `;
-        let data = document.querySelector('.input__date');
-        data.valueAsDate = new Date();
-        ApiApod(data);
-        data.addEventListener('change', () => {
-            ApiApod(data);
+        let date = document.querySelector('.input__date');
+        date.valueAsDate = new Date();
+        apiApod(date);
+        deleteLastHandler();
+        date.addEventListener('change', () => {
+            apiApod(date);
+            numLastHandler = 1;
         });
-        TypeOfBtn = 1;
+        typeOfBtn = 1;
     }
 });
 NeoWsbtn.addEventListener('click', () => {
